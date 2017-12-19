6bc698d3d7535fe2434507b824fe133b1c7f064f
kaetee425 <kingpang@outlook.com>
2017-12-19 12:32:30 -0800

    deploy

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+ 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+ 	connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     databse: 'burger_db'
   });
 }
+
 
 connection.connect(function(err){
 	if (err) {
